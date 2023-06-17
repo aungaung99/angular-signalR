c19198188a54f8b324ddc8ca5480427135b57bd1
Aung Naing Oo <57518163+aungaung99@users.noreply.github.com>
2023-06-18 01:54:26 +0630

    added constant ui

---
+++ src/app/shared/components/chat-skeleton/chat-skeleton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-skeleton',
+  templateUrl: './chat-skeleton.component.html',
+  styleUrls: ['./chat-skeleton.component.scss']
+})
+export class ChatSkeletonComponent {
+
+}
