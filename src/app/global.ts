fd8f41de277a368ee9238585e6b3a5058369dc66
Aung Naing Oo <57518163+aungaung99@users.noreply.github.com>
2023-06-18 23:27:23 +0630

    Update global.ts

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,5 +1,6 @@
 export const GlobalVarialbe = Object.freeze({
-  //BASE_API_V1_URL: 'http://192.168.1.8/api/v1',
-  BASE_API_V1_URL: 'https://analytics-api.quickfoodmm.com/api/v1',
-  BASE_HUBCONNECTION_URL: 'https://analytics.quickfoodmm.com'
+  BASE_API_V1_URL: 'http://192.168.1.8/api/v1',
+  //BASE_API_V1_URL: 'https://analytics-api.quickfoodmm.com/api/v1',
+  BASE_HUBCONNECTION_URL: 'https://localhost:44378'
+  //BASE_HUBCONNECTION_URL: 'https://analytics.quickfoodmm.com'
 });
